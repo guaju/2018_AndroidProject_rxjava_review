ec53dc9d1927a1c45af9d6a84456c5deefc6fbfc
guaju <guaju@126.com>
2018-03-05 08:56:35 +0800

    项目初始化

---
+++ app/src/main/java/com/guaju/a2018_androidproject_rxjava_review/MainActivity.java
@@ -0,0 +1,14 @@
+package com.guaju.a2018_androidproject_rxjava_review;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
